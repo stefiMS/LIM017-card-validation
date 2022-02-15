97aa2120ca618909bb38abfb036833c1fe8bf551
Stefani Martinez <tefi_154@hotmail.com>
2022-02-15 12:24:50 -0500

    termino de las funcionalidades del validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const pagInicio=document.querySelector(".pagInicio");
+const putName=document.getElementById("putName");
+const putLastName=document.getElementById("putLastName");
+const putDateValid= document.getElementById("putDateValid");
+const numberCard=document.getElementById("numberCard");
+const cardVisa=document.getElementById("cardVisa");
+const pagVisa=document.querySelector(".pagVisa");
+const numCardVisa=document.querySelector(".numCardVisa");
+const nameUser=document.querySelector(".nameUser");
+const validUntil=document.querySelector(".validUntil");
+//const btnValidCard=document.getElementById("btnValidCard");--- eliminar
+
+//Página de CardVisa
+cardVisa.addEventListener("click",() =>{
+    if (putName.value.length==0 && putLastName.value.length==0 && putDateValid.value.length==0 
+    && numberCard.value.length==0){
+        alert("Debes completar todos los campos solicitados")
+    }
+    else if(validator.isValid(numberCard.value)==true){
+        
+        pagInicio.classList.add("hide");
+        pagVisa.classList.remove("hide");
+        numCardVisa.innerHTML=validator.maskify(numberCard.value);
+        nameUser.innerHTML= putName.value + "  " + putLastName.value;
+        validUntil.innerHTML= "Válido hasta: " + putDateValid.value;
+        
+    }
+    
+})
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,50 @@
 const validator = {
-  // ...
-};
+  isValid: (numberCreditCard) =>{
+    const numCard = numberCreditCard;
+    const arr = numCard.split("");
+    const reverseArr = arr.reverse();
+    for(let i = 0; i<reverseArr.length; i++){
+      if(i%2==1){
+        reverseArr[i] = parseInt(reverseArr[i])*2;
+        if(reverseArr[i]<10){
+          reverseArr[i];
+        }
+        else{
+          reverseArr[i] = reverseArr[i]-9;
+        }
+      }
+      else{
+        reverseArr[i] = parseInt(reverseArr[i]);
+      }
+    }
+    let sumArr = 0;
+    for(let n=0; n<reverseArr.length; n++){
+     
+      sumArr += reverseArr[n];
+    }
+    if(sumArr%10==0){
+      return true;
+    }
+    else{
+       return false;
+    }
+  },
+  maskify: (numberCreditCard)=>{
+    const numCard=numberCreditCard;
+    const sliceMask=numCard.slice(0,-4);
+    let michi="";
+    for (let i=0; i<sliceMask.length;i++){
+      michi+="#";
+    }
+    const numberMask=numCard.replace(sliceMask,michi);
+    return numberMask;
+  }
+
+
+  
+
+}
+
 
 export default validator;
+
