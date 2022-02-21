7c745251c98569167909bc2417f118f804ff6ae6
Stefani Martinez <tefi_154@hotmail.com>
2022-02-21 12:05:34 -0500

    estructura de html

--- src/index.js
+++ src/index.js
@@ -1,33 +1,49 @@
 import validator from './validator.js';
 
-console.log(validator);
+// console.log(validator);
 
-const pagInicio=document.querySelector(".pagInicio");
-const putName=document.getElementById("putName");
-const putLastName=document.getElementById("putLastName");
-const putDateValid= document.getElementById("putDateValid");
-const numberCard=document.getElementById("numberCard");
-const cardVisa=document.getElementById("cardVisa");
-const pagVisa=document.querySelector(".pagVisa");
-const numCardVisa=document.querySelector(".numCardVisa");
-const nameUser=document.querySelector(".nameUser");
-const validUntil=document.querySelector(".validUntil");
+// const pagInicio=document.querySelector(".pagInicio");
+// const putName=document.getElementById("putName");
+// const putLastName=document.getElementById("putLastName");
+// const putDateValid= document.getElementById("putDateValid");
+// const numberCard=document.getElementById("numberCard");
+// const cardVisa=document.getElementById("cardVisa");
+// const pagVisa=document.querySelector(".pagVisa");
+// const numCardVisa=document.querySelector(".numCardVisa");
+// const nameUser=document.querySelector(".nameUser");
+// const validUntil=document.querySelector(".validUntil");
+// const validCard=document.getElementsByClassName("validCard");
+// const invalidCard=document.getElementsByClassName("invalidCard");
+
 //const btnValidCard=document.getElementById("btnValidCard");--- eliminar
 
 //Página de CardVisa
-cardVisa.addEventListener("click",() =>{
-    if (putName.value.length==0 && putLastName.value.length==0 && putDateValid.value.length==0 
-    && numberCard.value.length==0){
-        alert("Debes completar todos los campos solicitados")
-    }
-    else if(validator.isValid(numberCard.value)==true){
+// pagInicio.classList
+// pagVisa.classList.add("hide");
+// validCard.innerText.classList.add("hide");
+// invalidCard.innerText.classList.add("hide");
+// pagInicio.classList.remove("hide");
+// pagVisa.classList.add("hide");
+// cardVisa.addEventListener("click",() =>{
+   
+//     if (putName.value.length==0 && putLastName.value.length==0 && putDateValid.value.length==0 
+//     && numberCard.value.length==0){
         
-        pagInicio.classList.add("hide");
-        pagVisa.classList.remove("hide");
-        numCardVisa.innerHTML=validator.maskify(numberCard.value);
-        nameUser.innerHTML= putName.value + "  " + putLastName.value;
-        validUntil.innerHTML= "Válido hasta: " + putDateValid.value;
+//     }
+//     else if(validator.isValid(numberCard.value)==true){
+//         alert("Tarjeta válida");
+//         pagInicio.classList.add("hide");
+//         pagVisa.classList.remove("hide");
+//         numCardVisa.innerHTML=validator.maskify(numberCard.value);
+//         nameUser.innerHTML= putName.value + "  " + putLastName.value;
+//         validUntil.innerHTML= "Válido hasta: " + putDateValid.value;
         
-    }
-    
-})
+//     }
+//     else if(validator.isValid(numberCard.value)==false){
+//         alert("Tarjeta Inválida");
+        
+//     }
+//     else{
+
+//     }
+// })
